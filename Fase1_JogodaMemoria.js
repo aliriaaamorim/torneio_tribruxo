675be617a444ae18238e1d88d6ec6d912d89b414
aliriaaamorim <aliria.cruz@dcomp.ufs.br>
2021-12-08 05:59:30 -0300

    Versao 1.3.0.0
    Action touch card complete

--- Fase1_JogodaMemoria.js
+++ Fase1_JogodaMemoria.js
@@ -1,1 +1,50 @@
 // Primeira fase do jogo
+
+const placar = document.querySelector("#placar");
+const matriz = document.querySelector("#matriz_jogo");
+const imagens = [
+    'card_01.jpg',
+    'card_02.png',
+    'card_03.png',
+    'card_04.png',
+    'card_05.png',
+    'card_06.png',
+    'card_07.png',
+    'card_08.jpg'
+
+]
+
+let cartaHTLM = '';
+
+// associa cada imagem a uma capa 
+imagens.forEach(img => {
+    cartaHTLM += `
+        <div class="carta">
+            <img class ="frente" src="img/${img}" height="270" width="200">
+            <img class ="verso" src="img/card_capa.jpg" height="270" width="200">
+        </div>
+    `
+});
+
+matriz.innerHTML = cartaHTLM + cartaHTLM;
+
+const cartas = document.querySelectorAll('.matriz');
+
+
+function virarCarta() {
+    this.classList.add('abrir');
+}
+cartas.forEach(cart => cart.addEventListener("click", virarCarta));
+
+
+/*=========== Pontuação dos jogadores =============*/
+/*=================================================*/
+
+let nomeJogador1 = "Player-1";  // usar o nome definido no menu do jogo
+let pontosJogador1 = document.getElementById("nomeJogador1");
+
+let nomeJogador2 = "Player-2";  // usar o nome definido no menu do jogo
+let pontosJogador2 = 0;
+
+
+

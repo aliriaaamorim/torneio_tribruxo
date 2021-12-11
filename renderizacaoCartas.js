347d5fd0ea3e4194749f13d091cadc407e6ea82b
aliriaaamorim <aliria.cruz@dcomp.ufs.br>
2021-12-11 15:47:22 -0300

    Mudança na resolução das cartas

--- renderizacaoCartas.js
+++ renderizacaoCartas.js
@@ -3,22 +3,26 @@
 const placar = document.querySelector("#placar");
 const matriz = document.querySelector("#matriz_jogo");
 const imagens = [
-    'card_01.jpg',
+    'card_01.png',
     'card_02.png',
     'card_03.png',
-    'card_04.png',
+    'card_04.jpg',
     'card_05.png',
     'card_06.png',
-    'card_07.png',
-    'card_08.jpg'
+    'card_07.jpg',
+    'card_08.jpg',
+    'card_09.jpg',
+    'card_10.png',
+    'card_11.png',
+    'card_12.png'
 
 ]
 
-let cartaHTLM = '';
+let divCarta = '';
 
 // associa cada imagem a uma capa 
 imagens.forEach(img => {
-    cartaHTLM += `
+    divCarta += `
         <div class="carta">
             <img class ="frente" src="img/${img}" height="270" width="200">
             <img class ="verso" src="img/card_capa.jpg" height="270" width="200">
@@ -26,25 +30,52 @@ imagens.forEach(img => {
     `
 });
 
-matriz.innerHTML = cartaHTLM + cartaHTLM;
+matriz.innerHTML = divCarta + divCarta;
 
 const cartas = document.querySelectorAll('.carta');
 
-function virarCarta() {
+function virarCarta() {     /* Rotacao das cartas */
     this.classList.add('abrir');
 }
-cartas.forEach(cart => cart.addEventListener('click', virarCarta));
+cartas.forEach(item => item.addEventListener('click', virarCarta));
 
 
 
 /*=========== Pontuação dos jogadores =============*/
 /*=================================================*/
 
+let arrayCartas = [];
+
+//array.push() -> insere no final
+//array.pop() -> remove do final
+//array.shift() -> remove do inicio
+//array.unshift() - insere no inicio
+/*
+-----------
+*1ª carta aberta*
+    -se a 2ª for igual(mesmo nome .frente)
+        {
+            carta1 e carta2 aberta==true;
+            pula vez=false
+            this pontosJogador += 1
+        }
+        se não
+        {
+            carta1 e carta2 aberta==false
+            pula vez==true;
+        }
+    -não abrir mais de 2 cartas por vez
+*/
+
+function pulaVez(p1, p2) {    
+
+}
+
 let nomeJogador1 = "Player-1";  // usar o nome definido no menu do jogo
-let pontosJogador1 = document.getElementById("nomeJogador1");
+let pontosJogador1 = document.getElementById("pontuacao1");
 
 let nomeJogador2 = "Player-2";  // usar o nome definido no menu do jogo
-let pontosJogador2 = 0;
+let pontosJogador2 = document.getElementById("pontuacao2");
 
 
 

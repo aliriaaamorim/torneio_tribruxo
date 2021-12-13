5b8e32d742d8d87271d1b5a09ab70a01fa632360
aliriaaamorim <aliria.cruz@dcomp.ufs.br>
2021-12-13 10:25:08 -0300

    Comparacao das cartas e pontuação dos jogadores

--- renderizacaoCartas.js
+++ renderizacaoCartas.js
@@ -15,23 +15,30 @@ let imagens = [
     'card_10.png',
     'card_11.png',
     'card_12.png',
-    'card_01.2.png',
-    'card_02.2.png',
-    'card_03.2.png',
-    'card_04.2.jpg',
-    'card_05.2.png',
-    'card_06.2.png',
-    'card_07.2.jpg',
-    'card_08.2.jpg',
-    'card_09.2.jpg',
-    'card_10.2.png',
-    'card_11.2.png',
-    'card_12.2.png'
+    'card_13.png',
+    'card_14.png',
+    'card_15.png',
+    'card_01.png',
+    'card_02.png',
+    'card_03.png',
+    'card_04.jpg',
+    'card_05.png',
+    'card_06.png',
+    'card_07.jpg',
+    'card_08.jpg',
+    'card_09.jpg',
+    'card_10.png',
+    'card_11.png',
+    'card_12.png',
+    'card_13.png',
+    'card_14.png',
+    'card_15.png',
+
 ]
 
-/* randomizar elementos do imagens 'imagens' */
+/* randomizar elementos do array 'imagens' */
 
-function embaralharCartas(lista) {
+function embaralharCartas(lista) {  //código original(java): devmedia
 
     for (let itemLista = 0; itemLista < (lista.length) -1 ; itemLista++) {
         let aleatorio = Math.floor(Math.random()*itemLista);
@@ -41,20 +48,20 @@ function embaralharCartas(lista) {
     }
 
     return lista;
-    
 }
 
 imagens = embaralharCartas(imagens);
 
-
+// recebe um Template String para atribuicao
+/// de cada carta a uma clase HTML
 let divCarta = '';
 
 // associa cada imagem a uma capa 
 imagens.forEach(img => {
     divCarta += `
-        <div class="carta">
-            <img class ="frente" src="img/${img}" height="270" width="200">
-            <img class ="verso" src="img/card_capa.jpg" height="270" width="200">
+        <div class="carta" data-id="${img}">
+            <img class ="frente" src="img/${img}" height="90" width="67">
+            <img class ="verso" src="img/card_capa.jpg" height="90" width="67">
         </div>
     `
 });
@@ -63,9 +70,124 @@ matriz.innerHTML = divCarta;
 
 const cartas = document.querySelectorAll('.carta');
 
-function virarCarta() {     /* Rotacao das cartas */
-    this.classList.add('abrir');
+//////////////////////////////////////////
+///////// Definicao das variaveis
+///////// de nome e pontuacao dos 
+///////// jogadores
+
+const nomeJogador1 = "Jogador1";
+const nomeJogador2 = "Jogador2";
+let pontosJogador1 = 0;
+let pontosJogador2 = 3;
+
+const nomeJ1_html = document.getElementById("nomeJogador1"); 
+const nomeJ2_html = document.getElementById("nomeJogador2");
+const pontosJ1_html = document.getElementById("pontuacao1"); 
+const pontosJ2_html = document.getElementById("pontuacao2");
+
+nomeJ1_html.innerHTML = nomeJogador1;
+nomeJ2_html.innerHTML = nomeJogador2;
+pontosJ1_html.innerHTML = pontosJogador1;
+pontosJ2_html.innerHTML = pontosJogador2;
+
+
+/////////////////////////////////////////////
+// Variaveis para as cartas em ordem de click
+let carta1, carta2;
+
+/////////////////////////////////////////////
+// Controle de cartas abertas por vez (2)
+let bloquar = false; 
+
+//////////////////////////////////////////////////////
+// Controle de cartas viradas para finalizacao do jogo
+let cartasViradas = 0; //
+
+/////////////////////////////////////////////
+// Controle de vez de jogada para cada player
+let vezJogador1 = true;
+let vezJogador2;
+
+///////////////////////////////////////////////////
+///////// FUNCAO PARA VIRAR CARTA AO CLICAR ///////
+///  chama a classe 'abrir' no arquivo "style.css"
+
+function virarCarta() {
+    if (bloquar) return false;
+    
+    /// chama a classe 'abrir' no arquivo "style.css"
+    this.classList.add('abrir'); //Rotacao das cartas
+
+    /// verifica se ja existem cartas abertas
+    // se false 'carta1' recebe a carta clicada
+    if (!carta1) {
+        carta1 = this;
+        return false;
+    }
+
+    /// se true 'carta2' recebe a carta clicada
+    carta2 = this;
+
+    // chama a funcao 'comparar'
+    comparar();
+
 }
+
+
+//////////////////////////////////////////////////////
+///////// FUNCAO PARA COMPARAR CARTAS CLICADAS ///////
+function comparar(){
+
+    // compara o identificador das cartas clicadas
+    /// a partir da atribuicao em 'divCarta':60
+    let cartasIguais = carta1.dataset.id === carta2.dataset.id;
+    
+    if (cartasIguais){
+        cartasViradas++; // incrementa a variavel cartasViradas para registrar o fim do jogo
+        limparVariavel(cartasIguais); // chama a funcao 'limparvariavel' com o parametro 'cartasIguais' = true
+
+    } else{
+        desabilitarCartas();
+    }
+
+    // chama a funcao 'desabilitarCartas', caso os
+    ///identificadores sejam diferentes
+}
+
+///////////////////////////////////////////////////////////////
+///////// FUNCAO DESABILITAR CARTAS NAO CORRESPONDENTES ///////
+
+function desabilitarCartas(){
+    bloquar = true;
+    setTimeout(() => {
+        carta1.classList.remove('abrir');
+        carta2.classList.remove('abrir');
+
+        limparVariavel();
+
+    }, 1000);
+}
+
+////////////////////////////////////////////////////////////
+///////// FUNCAO DESVIRAR CARTAS NAO CORRESPONDENTES ///////
+
+function limparVariavel(cartasIguais = false){
+
+    //caso sejam iguais, as cartas permanecem abertas
+    if(cartasIguais){
+        carta1.removeEventListener('click', virarCarta);
+        carta2.removeEventListener('click', virarCarta);
+    }
+
+    // atrubui valores default para 'carta1' e 'carta2'
+    /// para receber as novas cartas
+    carta1 = null;
+    carta2 = null;
+    bloquar = false;
+}
+
+
+
 cartas.forEach(item => item.addEventListener('click', virarCarta));
 
 
@@ -73,39 +195,17 @@ cartas.forEach(item => item.addEventListener('click', virarCarta));
 /*=========== Pontuação dos jogadores =============*/
 /*=================================================*/
 
-let imagensCartas = [];
 
-//imagens.push() -> insere no final
-//imagens.pop() -> remove do final
-//imagens.shift() -> remove do inicio
-//imagens.unshift() - insere no inicio
-/*
------------
-*1ª carta aberta*
-    -se a 2ª for igual(mesmo nome .frente)
-        {
-            carta1 e carta2 aberta==true;
-            pula vez=false
-            this pontosJogador += 1
-        }
-        se não
-        {
-            carta1 e carta2 aberta==false
-            pula vez==true;
-        }
-    -não abrir mais de 2 cartas por vez
-*/
-
-function pulaVez(p1, p2) {    
-
+function pulaVez(p1, p2) {
 }
 
-let nomeJogador1 = "Player-1";  // usar o nome definido no menu do jogo
-let pontosJogador1 = document.getElementById("pontuacao1");
 
-let nomeJogador2 = "Player-2";  // usar o nome definido no menu do jogo
-let pontosJogador2 = document.getElementById("pontuacao2");
-
+function mudarCena(){
+  if (cartasViradas == 15){
+    // inserir a chamada da transição de cena
+    } 
+}
+  
 
 
 
